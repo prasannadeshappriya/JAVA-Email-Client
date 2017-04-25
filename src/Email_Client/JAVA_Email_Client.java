08bdaa4b6ad55bb06965c4de8dd4ee7d807d0da5
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-04-25 14:15:24 +0530

    Create Empty Project

---
+++ src/Email_Client/JAVA_Email_Client.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Email_Client;
+
+/**
+ *
+ * @author prasanna
+ */
+public class JAVA_Email_Client {
+    public static void main(String[] args) {
+        System.out.println("asdad");
+    }
+}
